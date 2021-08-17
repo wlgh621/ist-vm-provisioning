4d2c3cf16ff5f0f9b6517b5b38d64d887f8e216f
momagic5 <38769343+momagic5@users.noreply.github.com>
2021-08-17 14:16:08 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@
 output "vm_deploy" {
   value = [vsphere_virtual_machine.vm_deploy.*.name, vsphere_virtual_machine.vm_deploy.*.default_ip_address]
 }
+output "vm_ip" {
+  value = vsphere_virtual_machine.vm_deploy.*.default_ip_address
+}
+output "vm_name" {
+  value = vsphere_virtual_machine.vm_deploy.*.name
+}
