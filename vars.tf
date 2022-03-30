e04d49dc91eecfff07d37b4b8fe0a06a660d523e
cisgdemo <102663863+cisgdemo@users.noreply.github.com>
2022-03-30 19:52:10 +0900

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,44 +2,52 @@
 variable "vsphere_server" {
   type = string
   description = "Hostname or IP address of your vCenter server" 
+  default = "10.70.136.15"
 }
 
 variable "vsphere_user" {
   type = string
   description = "Username for vsphere"
+  default = "Administrator@VSPHERE.LOCAL"
 }
 
 variable "vsphere_password" {
   type = string
   description = "The password for vsphere"
+  default = "!234Qwer"
 }
 
 variable "datacenter" {
   type = string
   description = "The datacenter the resources will be created in"
+  default = "DC1"
 }
 
 
 variable "resource_pool" {
   type = string
   description = "Name of the resource pool"
+  default = "IST-Demo"
 }
 
 variable "datastore_name" {
   type = string
   description = "Name of the Datastore name"
+  default = "NAS"
 }
 
 // The name of the network to use.
 variable "network_name" {
   type = string
   description = "Name of the network"
+  default = "MGMT-Net_136.x"
 }
 
 // The name of the template to use when cloning.
 variable "template_name" {
   type = string
   description = "Name of the template"
+  default = "136.160Ubunutu2004"
 }
 
 
@@ -59,11 +67,13 @@ variable "vm_memory" {
 variable "vm_prefix" {
   type = string
   description = "The name prefix of the virtual machines to create"
+  default = "jenkins demo"
 }
 
 variable "vm_folder" {
   type = string
   description = "Name of folder"
+  default = "IST-Demo"
 }
 
 variable "vm_count" {
@@ -75,4 +85,5 @@ variable "vm_count" {
 variable "vm_domain" {
   type = string
   description = "Domain Name for the virtual machine"
+  default = "IST-Demo"
 }
