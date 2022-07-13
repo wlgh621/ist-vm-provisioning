7bb18aa9175de56aaad5587f4ad19b9de86b5fc1
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-13 11:45:02 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
-output "vm_deploy" {
-  value = [vsphere_virtual_machine.vm_deploy.*.name, vsphere_virtual_machine.vm_deploy.*.default_ip_address]
-}
-output "vm_ip" {
-  value = vsphere_virtual_machine.vm_deploy.*.default_ip_address
-}
-output "vm_name" {
-  value = vsphere_virtual_machine.vm_deploy.*.name
-}
+#output "vm_deploy" {
+#  value = [vsphere_virtual_machine.vm_deploy.*.name, vsphere_virtual_machine.vm_deploy.*.default_ip_address]
+#}
+#output "vm_ip" {
+#  value = vsphere_virtual_machine.vm_deploy.*.default_ip_address
+#}
+#output "vm_name" {
+#  value = vsphere_virtual_machine.vm_deploy.*.name
+#}
