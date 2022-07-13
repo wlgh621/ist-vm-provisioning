afdbc8e4dbbbda47af8ea7fc1316b266b6fdb9e4
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-13 22:11:49 +0900

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -9,22 +9,26 @@ data "vsphere_datacenter" "dc" {
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = var.datastore_name
+  name = local.vm[count.index].datastore_name
+  #name          = var.datastore_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = var.resource_pool
+  name = local.vm[count.index].resource_pool
+  #name          = var.resource_pool
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-  name          = var.network_name
+  name = local.vm[count.index].network_name
+  #name          = var.network_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = var.template_name
+  name = local.vm[count.index].template_name
+  #name          = var.template_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
