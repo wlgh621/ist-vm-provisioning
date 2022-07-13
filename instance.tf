11061145f32f4848178becccccbc2677ebb0be0f
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-13 22:54:44 +0900

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -46,7 +46,7 @@ resource "random_string" "folder_name_prefix" {
 
 
 resource "vsphere_folder" "vm_folder" {
-   count = length(local.vm)
+  count = length(local.vm)
   path          =  "${local.vm[count.index].vm_folder}-${random_string.folder_name_prefix.id}"
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.dc[count.index].id
@@ -60,28 +60,37 @@ resource "vsphere_virtual_machine" "vm_deploy" {
   #resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore[each.value.index].id
   #datastore_id     = data.vsphere_datastore.datastore.id
-  folder           = vsphere_folder.vm_folder.path
+  folder           =  vsphere_folder.vm_folder[each.value.index].path
+  #folder           = vsphere_folder.vm_folder.path
 
   num_cpus = each.value.vm_cpu
   memory   = each.value.memory
-  guest_id = data.vsphere_virtual_machine.template.guest_id
+  guest_id = data.vsphere_virtual_machine.template[each.value.index].guest_id
+  #guest_id = data.vsphere_virtual_machine.template.guest_id
 
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  scsi_type = data.vsphere_virtual_machine.template[each.value.index].scsi_type
+  #scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
-    network_id   = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    network_id   = data.vsphere_network.network[each.value.index].id
+    #network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template[each.value.index].network_interface_types[0]
+    #adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
   disk {
     label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks.0.size
-    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    size = data.vsphere_virtual_machine.template[each.value.index].disks.0.size
+    #size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template[each.value.index].disks.0.eagerly_scrub
+    #eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template[each.value.index].disks.0.thin_provisioned
+    #thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
   
   clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
+    template_uuid = data.vsphere_virtual_machine.template[each.value.index].id
+    #template_uuid = data.vsphere_virtual_machine.template.id
     customize {
       linux_options {
         host_name  = each.value.vm_prefix
